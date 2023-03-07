fd14f7b4281a86f51b53d160c1580b91b05a5e29
ZhuYaolin <540048506@qq.com>
2023-03-08 03:54:15 +0800

    Rename package

---
+++ NpsPy/__init__.py
@@ -0,0 +1,10 @@
+from NpsPy.kernel.custom import *
+from NpsPy.kernel.kernel import *
+from NpsPy.estimate.estimate import *
+from NpsPy.regression.regression import *
+'''
+custom: I, is_kernel
+Kernel: GaussianKernel, EpanechnikovKernel, tricubeKernel, boxcarKernel
+Estimate: NWEstimate, GMEstimate, LPEstimate
+Regression: NWRegression, GMRegression, LPRegression
+'''
